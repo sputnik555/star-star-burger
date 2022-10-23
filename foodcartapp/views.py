6cf9a70764f2c388386adf344014ceb1e7901f9e
Alexey Beletsky <sputnik2009@gmail.com>
2022-10-23 14:45:24 +0300

    make register_order view atomic

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.http import JsonResponse
 from django.templatetags.static import static
 
@@ -84,6 +85,7 @@ def product_list_api(request):
 
 
 @api_view(['POST'])
+@transaction.atomic
 def register_order(request):
     validate_order(request.data)
     order = Order.objects.create(
