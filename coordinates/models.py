da41942b48a1fff3a0e49bbbece971f579b39050
Alexey Beletsky <sputnik2009@gmail.com>
2022-10-31 01:27:45 +0300

    move the yandex api request in a separate module

--- coordinates/models.py
+++ coordinates/models.py
@@ -1,28 +1,10 @@
 import datetime
 
-from django.db import models
-import requests
 from django.conf import settings
+from django.db import models
 from django.utils import timezone
 
-
-def get_coordinates_from_yandex(address):
-    apikey = settings.YANDEX_GEO_TOKEN
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
+from coordinates.yandex_geo_api import get_coordinates
 
 
 class PlaceCoordinates(models.Model):
@@ -53,7 +35,7 @@ class PlaceCoordinates(models.Model):
         if self.update_date and timezone.now() - self.update_date < update_time_delta:
             return
 
-        coordinates = get_coordinates_from_yandex(self.address)
+        coordinates = get_coordinates(self.address)
         if coordinates:
             self.longitude, self.latitude = coordinates
             self.update_date = timezone.now()
@@ -61,4 +43,3 @@ class PlaceCoordinates(models.Model):
 
     def is_coordinates_filled(self):
         return self.longitude is not None and self.latitude is not None
-
